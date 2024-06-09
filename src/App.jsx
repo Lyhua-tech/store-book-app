902b345dd76ffb3f0ff8e0db50d61ea09775c180
Lyhua-tech <slyhour69@gmail.com>
2024-06-09 16:49:19 +0700

    add rest api

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import axios from 'axios';
+
 function App() {
   const [books, setBooks] = useState([]);
+
   const deleteBook = (id) => {
-    const removeBook = books.filter(item => {
-      return item.id !== id;
-    })
-    setBooks(removeBook)
+    const updatedBooks = books.filter(item => item.id !== id);
+    setBooks(updatedBooks);
   }
-  const createBook = (title) => {
-    const updateBook = [
-        ...books,
-       {id: Math.round(Math.random() * 9191), title}
-      ]
-    setBooks(updateBook)
+
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', { title });
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
   }
-  const handleUpdate = (id, title) => {
-    const updateBook = books.map(book => {
-      if(book.id === id) {
-        return { ...book, title};
-      } 
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const updateBook = async (id, title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, { title });
+    const updatedBooks = books.map(book => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
+      }
       return book;
     });
-    setBooks(updateBook)
+    setBooks(updatedBooks);
   }
+
   return (
-    <div >
-      <BookList books={books} onDelete={deleteBook} handleUpdate={handleUpdate}/>
+    <div>
+      <BookList books={books} onDelete={deleteBook} onUpdate={updateBook} />
       <BookCreate onCreate={createBook} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
